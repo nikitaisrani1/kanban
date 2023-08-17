7ca05029f3a4b83c2b37ffe697721ce993c52985
nikita <nikitaisrani1@gmail.com>
2023-08-17 23:24:49 +0530

    your message for this commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,23 +1,41 @@
 import React from "react";
-class Header extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedOption: null,
-        };
-    }
-render(){
+import './Header.css'
+import { useState } from "react";
+import { AiOutlineDown } from "react-icons/ai";
+import { TbAdjustmentsHorizontal } from "react-icons/tb";
+
+
+function Header() {
+    const [showList, setShowList] = useState(false);
+    
     return (
+        <div>
         <div className="header">
-            <div>
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-                <span class="material-symbols-outlined">tune</span>
-                <span>Display</span>
-                <span class="material-symbols-outlined">expand_more</span>
+            <div className="select" onClick={()=>setShowList(!showList)}>
+            <div className="divs"><TbAdjustmentsHorizontal /></div>
+            <div className="option">Display</div>
+                <div className="divs"><AiOutlineDown /></div>
             </div>
+            </div>
+            {showList &&
+            <div className="popup">
+                <div className="row">
+                    <div className="font">Grouping</div>
+                    <div className="selects">
+                        <AiOutlineDown />
+                </div>
+                </div>
+                    <div className="row">
+                        <div className="font">Ordering</div>
+                        <div className="selects">
+                            <AiOutlineDown />
+                        </div>
+                    </div>
+                </div>
+            }
         </div>
     );
-}
-}
+        }
+
 
 export default Header;
